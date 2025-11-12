601bd871e55c3fb3ef145b64d2e8879dbdac4902
Aditya Singh <adityasingh27072746@gmail.com>
2025-11-12 23:25:39 +0530

    minor changes 1

--- sports/urls.py
+++ sports/urls.py
@@ -5,14 +5,16 @@ app_name = 'sports'
 
 urlpatterns = [
     # Sport URLs
-    path('sports/', views.sport_list, name='sport-list'),
-    path('sports/<int:pk>/', views.sport_detail, name='sport-detail'),
+    path('sports/', views.sport_list, name='sport-list'), # will be handled in frontend
+    path('sports/<int:pk>/', views.sport_detail, name='sport-detail'), # will be handled in frontend
     
     # Registration URLs
-    path('registrations/', views.registration_list, name='registration-list'),
+    path('registrations/', views.registration_list, name='registration-list'), # Post feature is implemented, GET is to be worked up on
     path('registrations/<int:pk>/', views.registration_detail, name='registration-detail'),
     
     # Team URLs
     path('teams/', views.team_list, name='team-list'),
     path('teams/<int:pk>/', views.team_detail, name='team-detail'),
+
+    # TASK : Setup and endpoint to filter out registerations based on sport (use slugs)
 ]
